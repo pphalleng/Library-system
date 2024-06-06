a184ce5933d3d0385af8c1467db092ba46440215
Phoun Phalleng <phallengphoun@gmail.com>
2024-06-07 01:55:45 +0700

    init project

---
+++ myapp/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+var app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 80;
+
+// Import the customer routes
+const customerRoute = require("./route/customers");
+
+// Use the customers routes
+app.use('/api', customerRoute);
+
+app.get("/test",function(request,response){
+    response.send("Hello World!")
+})
+
+
+app.listen(port, () => {
+    // console.log("Started application on port http://localhost ");
+    console.log("Started application on port http://localhost ");
+})
