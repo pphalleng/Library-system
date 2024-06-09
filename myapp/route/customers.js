245d3d6cfd22dabc3aac1044f35d8bbc68ab5557
Administrator <lenghuy.hour@gmail.com>
2024-06-09 16:47:26 +0700

    Add field validation on Customer.js

--- myapp/route/customers.js
+++ myapp/route/customers.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const {check, validationResult} = require('express-validator');
 
 let customers = [
     { 
@@ -8,7 +9,7 @@ let customers = [
         last_name: 'Doe',
         age: 30,
         date_of_birth: '2002-01-01',
-        id_card_number: '123456789',
+        nid_passport_number: '123456789',
         current_address: 'kompongcham',
     },
     { 
@@ -17,7 +18,7 @@ let customers = [
         last_name: 'Smith',
         age: 25,
         date_of_birth: '2005-06-01',
-        id_card_number: '987654321',
+        nid_passport_number: '987654321',
         current_address: 'kampot',
     },
 ];
@@ -41,28 +42,36 @@ router.get('/customers/:id', (req, res) => {
 
 
 // Create a new Customer
-router.post('/customers', (req, res) => {
-    const { first_name, last_name, age,  date_of_birth, id_card_number, current_address} = req.body;
-  
+router.post('/create-customers', [check('date_of_birth').isDate()], (req, res) => {
+    const errors = validationResult(req);
+    const { first_name, last_name, age,  date_of_birth, nid_passport_number, current_address} = req.body;
     // Simple validation
-    if (!first_name || !last_name || !age || !date_of_birth || !id_card_number || !current_address) {
-      return res.status(400).json({ message: 'Name and age are required' });
+    if (!first_name || !last_name || !age || !date_of_birth || !nid_passport_number || !current_address) {
+      return res.status(400).json({ message: 'All fields are required, please check again to input them.' });
+    }
+    if(!errors.isEmpty()){
+        console.log(errors)
+        return res.status(422).json({errors: errors.array()})
     }
   
-    const newCustomer = { id: customers.length + 1, first_name, last_name, age, date_of_birth, id_card_number, current_address};
+    const newCustomer = { id: customers.length + 1, first_name, last_name, age, date_of_birth, nid_passport_number, current_address};
     customers.push(newCustomer);
   
     res.status(201).json(newCustomer);
 });
 
 // Update an existing Customer by ID
-router.put('/customers/:id', (req, res) => {
+router.put('/update-customers/:id', [check('date_of_birth').isDate()], (req, res) => {
+    const errors = validationResult(req);
     const { id } = req.params;
-    const { first_name, last_name, age,  date_of_birth, id_card_number, current_address} = req.body;
-  
+    const { first_name, last_name, age,  date_of_birth, nid_passport_number, current_address} = req.body;
     // Simple validation
-    if (!first_name || !last_name || !age || !date_of_birth || !id_card_number || !current_address) {
-      return res.status(400).json({ message: 'Name and age are required' });
+    if (!first_name || !last_name || !age || !date_of_birth || !nid_passport_number || !current_address) {
+      return res.status(400).json({ message: 'All fields are required, please check again to input them.' });
+    }
+    if(!errors.isEmpty()){
+        console.log(errors)
+        return res.status(422).json({errors: errors.array()})
     }
   
     const customer = customers.find((customer) => customer.id === parseInt(id));
@@ -71,18 +80,18 @@ router.put('/customers/:id', (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
   
-    customer.name = first_name;
+    customer.first_name = first_name;
     customer.last_name = last_name;
     customer.age = age;
     customer.date_of_birth = date_of_birth;
-    customer.id_card_number = id_card_number;
+    customer.nid_passport_number = nid_passport_number;
     customer.current_address = current_address;
   
     res.json(customer);
 });
 
 // Delete a user by ID
-router.delete('/customers/:id', (req, res) => {
+router.delete('/delete-customers/:id', (req, res) => {
     const { id } = req.params;
     customers = customers.filter((customer) => customer.id !== parseInt(id));
     res.sendStatus(204);
