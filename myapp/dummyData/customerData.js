3e1566ebb9cf9b19265d8aae9ad82755fac424e1
Phoun Phalleng <phallengphoun@gmail.com>
2024-06-09 16:38:46 +0700

    CRUD librarains

---
+++ myapp/dummyData/customerData.js
@@ -0,0 +1,98 @@
+[
+    {
+        "first_name": "Ea khun",
+        "last_name": "vibol",
+        "age": "21",
+        "nid_passport_number": "000001",
+        "current_address": "Phnom penh",
+        "": ""
+    },
+    {
+        "first_name": "Seyha",
+        "last_name": "Both",
+        "age": "16",
+        "nid_passport_number": "000002",
+        "current_address": "phnom penh",
+        "": ""
+    },
+    {
+        "first_name": "Virak",
+        "last_name": "panha",
+        "age": "11",
+        "nid_passport_number": "000003",
+        "current_address": "Kep",
+        "": ""
+    },
+    {
+        "first_name": "panha",
+        "last_name": "both",
+        "age": "19",
+        "nid_passport_number": "000004",
+        "current_address": "siem reap",
+        "": ""
+    },
+    {
+        "first_name": "vuth",
+        "last_name": "chhun",
+        "age": "22",
+        "nid_passport_number": "000005",
+        "current_address": "kandal",
+        "": ""
+    },
+    {
+        "first_name": "Seng",
+        "last_name": "Chhun",
+        "age": "20",
+        "nid_passport_number": "000006",
+        "current_address": "battambang",
+        "": ""
+    },
+    {
+        "first_name": "lee",
+        "last_name": "ma",
+        "age": "25",
+        "nid_passport_number": "000007",
+        "current_address": "phnom penh",
+        "": ""
+    },
+    {
+        "first_name": "lee",
+        "last_name": "xing",
+        "age": "18",
+        "nid_passport_number": "000008",
+        "current_address": "takeo",
+        "": ""
+    },
+    {
+        "first_name": "Long",
+        "last_name": "kim",
+        "age": "17",
+        "nid_passport_number": "000009",
+        "current_address": "pursat",
+        "": ""
+    },
+    {
+        "first_name": "Khun",
+        "last_name": "kin",
+        "age": "17",
+        "nid_passport_number": "000010",
+        "current_address": "pursat",
+        "": null
+    },
+    {
+        "first_name": "vathana",
+        "last_name": "seam",
+        "age": "16",
+        "nid_passport_number": "000011",
+        "current_address": "Phnom penh",
+        "": null
+    },
+    {
+        "first_name": "panha",
+        "last_name": "wath",
+        "age": "25",
+        "nid_passport_number": "000012",
+        "current_address": "mondulkiri",
+        "": null
+    }
+]
