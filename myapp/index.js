6591c592c40213f4e2e813ae918f5b0a44dd76f3
Phoun Phalleng <phallengphoun@gmail.com>
2024-06-11 00:19:37 +0700

    fixed route

--- myapp/index.js
+++ myapp/index.js
@@ -12,10 +12,16 @@ const port = process.env.PORT || 80;
 // Import the customer routes
 const customerRoute = require("./route/customers");
 const lirarainRoute = require("./route/librarain");
+const transactionRoute = require("./route/transactions");
+const transactionLineRoute = require("./route/transaction_lines");
+const bookRoute = require("./route/books");
 
 // Use the customers routes
 app.use('/api', customerRoute);
 app.use('/api', lirarainRoute);
+app.use('/api', transactionRoute);
+app.use('/api', transactionLineRoute);
+app.use('/api', bookRoute);
 
 app.get("/test",function(request,response){
     response.send("Hello everyone, Here is NodeJS project.")
